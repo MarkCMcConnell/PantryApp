2a5d00d13225784dfa8132b0f1943d1acdeee24f
Mark McConnell <mcmcconn@gmail.com>
2020-04-17 10:30:31 -0400

    Update packages

--- src/app/ingredients/IngredientList/IngredientList.vue
+++ src/app/ingredients/IngredientList/IngredientList.vue
@@ -1,35 +1,35 @@
 <template>
-    <div>
-        <ul>
-            <li
-                v-for="ingredient in ingredients"
-                :key="ingredient.name"
-                @click.prevent="showIngredient(ingredient.id)"
-            >
-                {{ ingredient.name }} - {{ ingredient.quantity }} {{ ingredient.unit }}
-            </li>
-        </ul>
-    </div>
+  <div>
+    <ul>
+      <li
+        v-for="ingredient in ingredients"
+        :key="ingredient.name"
+        @click.prevent="showIngredient(ingredient.id)"
+      >
+        {{ ingredient.name }} - {{ ingredient.quantity }} {{ ingredient.unit }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 import IngredientDetail from '../IngredientDetail/IngredientDetail.vue';
 
 export default {
-    components: {
-        IngredientDetail,
+  components: {
+    IngredientDetail,
+  },
+  data() {
+    return {
+      ingredients: this.$store.state.ingredients,
+    };
+  },
+  methods: {
+    showIngredient(id) {
+      this.$router.push({ path: `/ingredients/${id}` });
     },
-    data () {
-        return {
-            ingredients: this.$store.state.ingredients,
-        }
-    },
-    methods: {
-        showIngredient (id) {
-            this.$router.push({ path: `/ingredients/${id}` });
-        },
-    },
-}
+  },
+};
 </script>
 
 <style scoped>
